b0ac466a3bf337861e5f3e26c3dda81445107370
colin <colin@501a4a96-914b-0410-8454-f5988a4ca596>
2014-01-08 01:22:56 +0000

    Removing unneeded branch

--- root/branches/2.4.0_fast/modules/strategy/src/test/sample_data/inputs/EmptyStrategy.rb
+++
@@ -1,8 +0,0 @@
-java_import org.marketcetera.strategy.ruby.Strategy
-
-class EmptyStrategy < Strategy
-  def on_ask(ask)
-        set_property("onAsk",
-                     ask.toString());
-  end  
-end
