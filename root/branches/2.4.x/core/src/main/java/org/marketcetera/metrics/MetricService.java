dd63d62c3750911da6d13d0abc012d3bc6e1ab49
colin <colin@501a4a96-914b-0410-8454-f5988a4ca596>
2017-04-28 14:42:22 +0000

    Create 2.4.x branch

---
+++ root/branches/2.4.x/core/src/main/java/org/marketcetera/metrics/MetricService.java
@@ -0,0 +1,49 @@
+package org.marketcetera.metrics;
+
+import com.codahale.metrics.MetricRegistry;
+
+/* $License$ */
+
+/**
+ * Provides metric services.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class MetricService
+{
+    /**
+     * Create a new MetricService instance.
+     */
+    public MetricService()
+    {
+        metrics = new MetricRegistry();
+    }
+    /**
+     * Get the singleton instance of the metric service.
+     *
+     * @return a <code>MetricService</code> value
+     */
+    public static MetricService getInstance()
+    {
+        return instance;
+    }
+    /**
+     * Get the underlying MetricRegistry value.
+     *
+     * @return a <code>MetricRegistry</code> value
+     */
+    public MetricRegistry getMetrics()
+    {
+        return metrics;
+    }
+    /**
+     * singleton metrics instance
+     */
+    private static MetricService instance = new MetricService();
+    /**
+     * metrics registry
+     */
+    private final MetricRegistry metrics;
+}
