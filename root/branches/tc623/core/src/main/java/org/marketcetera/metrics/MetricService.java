e9317882a39f5a53a801e47643acedde87982c3f
colin <colin@501a4a96-914b-0410-8454-f5988a4ca596>
2017-06-15 20:29:56 +0000

    TC-623 create test branch

---
+++ root/branches/tc623/core/src/main/java/org/marketcetera/metrics/MetricService.java
@@ -0,0 +1,52 @@
+package org.marketcetera.metrics;
+
+import org.springframework.stereotype.Service;
+
+import com.codahale.metrics.MetricRegistry;
+
+/* $License$ */
+
+/**
+ * Provides metric services.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@Service
+public class MetricService
+{
+    /**
+     * Create a new MetricService instance.
+     */
+    public MetricService()
+    {
+        metrics = new MetricRegistry();
+    }
+    /**
+     * Get the singleton instance of the metric service.
+     *
+     * @return a <code>MetricService</code> value
+     */
+    public static MetricService getInstance()
+    {
+        return instance;
+    }
+    /**
+     * Get the underlying MetricRegistry value.
+     *
+     * @return a <code>MetricRegistry</code> value
+     */
+    public MetricRegistry getMetrics()
+    {
+        return metrics;
+    }
+    /**
+     * singleton metrics instance
+     */
+    private static MetricService instance = new MetricService();
+    /**
+     * metrics registry
+     */
+    private final MetricRegistry metrics;
+}
