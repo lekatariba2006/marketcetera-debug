a733211982de4a29aec3ec78a1b6bd5daf2ffa26
colin <colin@501a4a96-914b-0410-8454-f5988a4ca596>
2018-11-15 01:50:27 +0000

    Rename 4.0.x branch

--- eventbus/eventbus-core/src/test/java/org/marketcetera/eventbus/EventBusTestBase.java
+++ eventbus/eventbus-core/src/test/java/org/marketcetera/eventbus/EventBusTestBase.java
@@ -52,7 +52,9 @@ public abstract class EventBusTestBase<ServiceClazz extends EventBusService>
     public void cleanup()
             throws Exception
     {
-        eventBusService.unregister(this);
+        try {
+            eventBusService.unregister(this);
+        } catch (Exception ignored) {}
     }
     /**
      * Test event bus throughput.
@@ -93,6 +95,7 @@ public abstract class EventBusTestBase<ServiceClazz extends EventBusService>
         EventBusTestEvent testEvent1 = new EventBusTestEvent();
         eventBusService.post(testEvent1);
         verifySameTestEvent(testEvent1);
+        eventBusService.unregister(this);
     }
     /**
      * Receive test event values.
